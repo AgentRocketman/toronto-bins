d480be46dcbaaa241e8fedc7a2d5ce27a2719500
AgentRocketman <agentrocketman@users.noreply.github.com>
2026-06-28 04:46:00 +0000

    Debug: log actual OpenRouter response structure

--- public_html/mission-control/api/balances.php
+++ public_html/mission-control/api/balances.php
@@ -29,10 +29,21 @@ if (defined('MC_OPENROUTER_KEY') && MC_OPENROUTER_KEY) {
     
     if ($result) {
         $data = json_decode($result, true);
+        // Debug: log actual response
+        $response['_debug_openrouter_keys'] = array_keys($data);
+        if (isset($data['data'])) {
+            $response['_debug_data_keys'] = array_keys($data['data']);
+        }
+        
+        // Try multiple possible locations for balance
         if (isset($data['data']['balance'])) {
             $response['balances']['kimi'] = floatval($data['data']['balance']);
+        } elseif (isset($data['balance'])) {
+            $response['balances']['kimi'] = floatval($data['balance']);
+        } elseif (isset($data['credit_balance'])) {
+            $response['balances']['kimi'] = floatval($data['credit_balance']);
         } else {
-            $response['balances']['errors'][] = 'OpenRouter: No balance in response';
+            $response['balances']['errors'][] = 'OpenRouter: ' . json_encode($data);
         }
     } else {
         $response['balances']['errors'][] = 'OpenRouter: Failed to fetch';
